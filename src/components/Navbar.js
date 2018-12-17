4e5271f85967b3db9a6299b4b7c78457c71c58d1
elodeesign <e.gwizdek@c-lacom.fr>
2018-12-17 15:11:22 +0100

    styled component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
+
+const MainContainer = styled.div`
+    padding: 1em;
+    border-bottom: 0.1em solid #dedede;
+`
+
+const LeftPart = styled.div``
+
+const RightPart = styled.div``
 
 class Navbar extends Component {
     render() {
-        return <div style={{ backgroundColor: 'pink' }}>NAVBAR</div>
+        return (
+            <MainContainer>
+                <LeftPart>Accueil</LeftPart>
+                <RightPart>Contact</RightPart>
+            </MainContainer>
+        )
     }
 }
 
