9c260eaac0d43fe038fdb9d2a55380cbc1f336d9
elodeesign <e.gwizdek@c-lacom.fr>
2018-12-19 11:29:50 +0100

    api

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -3,32 +3,74 @@ import React, { Component, Fragment } from 'react'
 import { Splitter } from '../components'
 
 class Landing extends Component {
-    state = {}
+    state = { movies: [] }
+
+    async componentDidMount() {
+        let response = await fetch(
+            'https://api.themoviedb.org/3/discover/movie?api_key=b92715922f04621de38d69cf55169453'
+            //'https://api.pokemontcg.io/v1/cards
+        )
+        let data = await response.json()
+        console.log('movies', data)
+
+        // Préparation de movies
+        const movies = data.results.map((movie, index) => {
+            // alterner les blocks
+            if (index % 2 == 0) {
+                return {
+                    imageLeft:
+                        'http://image.tmdb.org/t/p/original/' +
+                        movie.poster_path,
+                    rightContent: movie.title
+                }
+            } else {
+                return {
+                    imageRight:
+                        'http://image.tmdb.org/t/p/original/' +
+                        movie.poster_path,
+                    leftContent: movie.title
+                }
+            }
+        })
+        this.setState({ movies })
+    }
 
     render() {
+        // const landing_parts = [
+        //     {
+        //         key: 'header',
+        //         leftContent: (
+        //             <span>
+        //                 Je suis
+        //                 <br />
+        //                 IRON MAN
+        //             </span>
+        //         ),
+        //         imageRight:
+        //             'https://cdn.shopify.com/s/files/1/1481/4622/products/mv68308_Iron_man_40_1024x1024.jpg?v=1501281970'
+        //     },
+        //     {
+        //         key: 'firstPart',
+        //         rightContent: (
+        //             <span>
+        //                 L’intelligence
+        //                 <br /> ce n’est pas un privilège,
+        //                 <br /> c’est un don.
+        //             </span>
+        //         ),
+        //         imageLeft:
+        //             'https://sm.ign.com/ign_fr/news/s/spider-man/spider-man-ps4-becomes-marvel-comics-canon-thanks-to-spiderg_ftc3.jpg'
+        //     }
+        // ]
+
+        const { movies } = this.state
+
         return (
             <Fragment>
                 <h1>Landing Page</h1>
-                <Splitter
-                    imageRight="https://cdn.shopify.com/s/files/1/1481/4622/products/mv68308_Iron_man_40_1024x1024.jpg?v=1501281970"
-                    leftContent={
-                        <span>
-                            Je suis
-                            <br />
-                            IRON MAN
-                        </span>
-                    }
-                />
-                <Splitter
-                    rightContent={
-                        <span>
-                            L’intelligence
-                            <br /> ce n’est pas un privilège,
-                            <br /> c’est un don.
-                        </span>
-                    }
-                    imageLeft="https://www.sideshowtoy.com/assets/products/300554-spider-man-miles-morales/lg/marvel-spider-man-miles-morales-premium-format-figure-sideshow-300554-13.jpg"
-                />
+                {movies.map((part, index) => {
+                    return <Splitter {...part} />
+                })}
             </Fragment>
         )
     }
